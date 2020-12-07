c157f4581a8396d0549090b9fbaab49e85c0b24d
zengzhiwei <308164213@qq.com>
2020-12-07 19:56:30 +0800

    + Log

---
+++ Assets/Standard Assets/Engine/Log/Log.cs
@@ -0,0 +1,24 @@
+﻿#region Copyright © 2018 Aver. All rights reserved.
+/*
+=====================================================
+ AverFrameWork v1.0
+ Filename:    Log.cs
+ Author:      Zeng Zhiwei
+ Time:        2019/12/8 18:04:55
+=====================================================
+*/
+#endregion
+
+public static class Log
+{
+    public static void Debug(string message)
+    {
+        UnityEngine.Debug.Log(message);
+    }
+
+    public static void DebugFormat(string format,params object[] param)
+    {
+        UnityEngine.Debug.LogFormat(format, param);
+    }
+
+}
