92ac95d9d7e7cd15d1b418b8e298c8b01296ba0b
zengzhiwei <308164213@qq.com>
2020-12-07 19:57:27 +0800

    + 测试场景TestNet

---
+++ Assets/Scripts/Test/TestNet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestNet : MonoBehaviour
+{
+    public Button m_btn;
+    void Start()
+    {
+        // 初始化按钮
+        m_btn = GameObject.Find("Button").GetComponent<Button>();
+        if(m_btn)
+        {
+            m_btn.onClick.AddListener(OnBtnClick);
+        }
+
+        // 初始化连接
+        ClientSocket.Instance.Connect();
+
+        // 注册
+        NetMsg.AddListener(Network.OpCode.S2C_TestResponse, new NetHandler(Test));
+    }
+
+    private void Test(string msg)
+    {
+        Log.Debug(msg);
+    }
+
+    private void OnBtnClick()
+    {
+        NetMsg.SendMsg(new NetMsgData(Network.OpCode.C2S_TestRequest, "1111"));
+    }
+}
