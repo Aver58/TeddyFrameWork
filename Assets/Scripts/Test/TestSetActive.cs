3eb0ddb4820d39b132a9cdf82207bae23bf3e29f
aver58 <308164213@qq.com>
2020-12-21 23:52:11 +0800

    + Test SetActive 还是有GC

---
+++ Assets/Scripts/Test/TestSetActive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSetActive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            gameObject.SetActive(false);
+            gameObject.SetActive(true);
+            // MaskableGraphic.OnEnable 有gc
+        }
+    }
+}
