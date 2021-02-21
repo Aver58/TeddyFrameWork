843964fca09d486988774d3e5b2abb48a8510e4c
aver58 <308164213@qq.com>
2021-02-22 00:19:32 +0800

    + UIExporter

---
+++ Assets/Standard Assets/Engine/UI/UIHierarchy/UICollection.cs
@@ -0,0 +1,27 @@
+﻿#region Copyright © 2020 Aver. All rights reserved.
+/*
+=====================================================
+ AverFrameWork v1.0
+ Filename:    UICollection.cs
+ Author:      Zeng Zhiwei
+ Time:        2021\2\19 星期五 21:59:58
+=====================================================
+*/
+#endregion
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UICollection组件集合
+/// </summary>
+public sealed class UICollection : MonoBehaviour
+{
+    [SerializeField]
+    internal List<Component> components = new List<Component>();
+    public T GetComponent<T>(int index) where T : Object
+    {
+        return components[index] as T;
+    }
+}
