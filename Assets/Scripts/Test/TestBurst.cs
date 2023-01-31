4c61ab665f373b4ddae462b36acc055081920eed
曾志伟 <zengzhiwei@xmfunny.com>
2023-01-31 23:43:42 +0800

    + TestBurst

---
+++ Assets/Scripts/Test/TestBurst.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class MyBurst2Behavior : MonoBehaviour {
+    void Start()
+    {
+        var input = new NativeArray<float>(10, Allocator.Persistent);
+        var output = new NativeArray<float>(1, Allocator.Persistent);
+        for (int i = 0; i < input.Length; i++)
+            input[i] = 1.0f * i;
+
+        var job = new MyJob
+        {
+            Input = input,
+            Output = output
+        };
+        job.Schedule().Complete();
+
+        Debug.Log("The result of the sum is: " + output[0]);
+        input.Dispose();
+        output.Dispose();
+    }
+
+    // Using BurstCompile to compile a Job with burst
+    // Set CompileSynchronously to true to make sure that the method will not be compiled asynchronously
+    // but on the first schedule
+    [BurstCompile(CompileSynchronously = true)]
+    private struct MyJob : IJob
+    {
+        [ReadOnly]
+        public NativeArray<float> Input;
+
+        [WriteOnly]
+        public NativeArray<float> Output;
+
+        public void Execute()
+        {
+            float result = 0.0f;
+            for (int i = 0; i < Input.Length; i++)
+            {
+                result += Input[i];
+            }
+            Output[0] = result;
+        }
+    }
+}
---
+++ Assets/Scripts/Test/TestPhysics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TestPhysics : MonoBehaviour {
+    void Start() {
+
+
+    }
+
+    Action onDrawGizmos;
+    private void OnDrawGizmos() {
+        onDrawGizmos?.Invoke();
+        onDrawGizmos = null;
+    }
+
+    RaycastHit[] hits = new RaycastHit[10];
+    void Update() {
+        Array.Clear(hits, 0, 10);
+        Physics.SphereCastNonAlloc(transform.position, 0.5f, Vector3.down, hits,
+            2, LayerMask.GetMask("Default"), QueryTriggerInteraction.Ignore);
+        for (int i = 0; i < hits.Length; i++) {
+            var hit = hits[i];
+            if (hit.transform != null) {
+                onDrawGizmos += () => {
+                    Handles.Label(transform.position + new Vector3(0,1,0) , "hit" + hit.transform);
+                    Handles.Label(transform.position + new Vector3(0,1,0) , "hit Normal" + hit.normal);
+
+                    Debug.DrawLine(transform.position + hit.normal, transform.position + hit.normal *2,Color.green);
+                };
+                break;
+            }
+        }
+
+    }
+}
