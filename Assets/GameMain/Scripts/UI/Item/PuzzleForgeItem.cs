ffc2e000978ced289212bd7d451c404e20cc8ac9
曾志伟 <308164213@qq.com>
2024-01-11 00:00:36 +0800

    + 格子合成算法，还没调试完

---
+++ Assets/GameMain/Scripts/UI/Item/PuzzleForgeItem.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class PuzzleForgeItem : MonoBehaviour {
+    private const int MinMergeCount = 3;
+    public int RowIndex;
+    public int ColumnIndex;
+    public int Index;
+    public int Level;  // 当前格子存储的资源等级
+    public List<PuzzleForgeItem> NeighborGrids;
+    private List<PuzzleForgeItem> toClearGrids;
+    
+    
+    [SerializeField] private Image ImgBg;
+    [SerializeField] private Image ImgIcon;
+    [SerializeField] private Text TxtLevel;
+    [SerializeField] private Button BtnClick;
+
+    #region Public
+
+    public void Init(int rowIndex, int columnIndex, int index) {
+        Level = 0;
+        RowIndex = rowIndex;
+        ColumnIndex = columnIndex;
+        Index = index;
+        NeighborGrids = new List<PuzzleForgeItem>();
+        toClearGrids = new List<PuzzleForgeItem>(2);
+        
+        BtnClick.onClick.AddListener(OnBtnClick);
+    }
+
+    public void Clear() {
+        BtnClick.onClick.RemoveListener(OnBtnClick);
+        
+    }
+    
+    public void AddNeighbor(PuzzleForgeItem item) {
+        NeighborGrids.Add(item);
+    }
+
+    public void ClearLevel() {
+        Level = 0;
+        TxtLevel.text = Level.ToString();
+    }
+    
+    #endregion
+
+    #region Private
+
+    private void OnBtnClick() {
+        if (Level > 0) {
+            return;
+        }
+
+        LevelUp();
+        RequestMerge();
+    }
+
+    private void RequestMerge() {
+        var count = 1;
+        toClearGrids.Clear();
+        for (int i = 0; i < NeighborGrids.Count; i++) {
+            var grid = NeighborGrids[i];
+            if (grid.Level == Level) {
+                count++;
+                toClearGrids.Add(grid);
+                // 遍历第二层
+                for (int j = 0; j < grid.NeighborGrids.Count; j++) {
+                    var grid2 = grid.NeighborGrids[j];
+                    if (grid2.Index != Index && 
+                        grid2.Index != grid.Index && 
+                        grid2.Level == Level) {
+                        count++;
+                        toClearGrids.Add(grid2);
+                    }
+                }
+            }
+        }
+
+        if (count >= MinMergeCount) {
+            LevelUp();
+            // 死循环预警
+            RequestMerge();
+        }
+    }
+
+    private void LevelUp() {
+        Level++;
+        // 先用数字代替吧
+        TxtLevel.text = Level.ToString();
+        for (int i = 0; i < toClearGrids.Count; i++) {
+            var grid = toClearGrids[i];
+            grid.ClearLevel();
+        }
+
+        toClearGrids.Clear();
+        // 设置图片
+        // ImgIcon.sprite = "";
+    }
+
+    #endregion
+}
--- Assets/GameMain/Scripts/UI/PuzzleForgeForm.cs
+++ Assets/GameMain/Scripts/UI/PuzzleForgeForm.cs
@@ -1,10 +1,71 @@
-﻿using UnityEngine;
-using UnityGameFramework.Runtime;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PuzzleForgeForm : FullScreenForm {
+    private const int GridRowCount = 5;
+    private const int GridColumnCount = 6;
+    [SerializeField] private Transform GridParent;
+    [SerializeField] private GameObject GridItemTemplate;
+    
+    // todo 数据层
+    // PuzzleForgeController
+    private List<PuzzleForgeItem> gridItemMap;
+
     protected override void OnInit(object userData) {
         base.OnInit(userData);
+        LoadAllGrid();
+    }
 
-        Debug.LogError("OnInit");
+    private void LoadAllGrid() {
+        gridItemMap = new List<PuzzleForgeItem>(GridRowCount * GridColumnCount);
+        
+        for (int row = 0; row < GridRowCount; row++) {
+            for (int column = 0; column < GridColumnCount; column++) {
+                var go = Instantiate(GridItemTemplate, GridParent);
+                var gridItem = go.GetComponent<PuzzleForgeItem>();
+                if (gridItem != null) {
+                    gridItem.Init(row, column, GetIndex(row, column));
+                    gridItemMap.Add(gridItem);
+                }
+            }
+        }
+        
+        // 初始化格子邻居
+        for (int i = 0; i < gridItemMap.Count; i++) {
+            var item = gridItemMap[i];
+            InitAllNeighbor(item);
+        }
+    }
+
+    private int GetIndex(int rowIndex, int columnIndex) {
+        return rowIndex * GridColumnCount + columnIndex;
+    }
+    
+    private void InitAllNeighbor(PuzzleForgeItem item) {
+        var rowIndex = item.RowIndex;
+        var columnIndex = item.ColumnIndex;
+        // 上
+        if (rowIndex - 1 >= 0) {
+            var upItem = gridItemMap[GetIndex(rowIndex - 1, columnIndex)];
+            item.AddNeighbor(upItem);
+        }
+        
+        // 下
+        if (rowIndex + 1 < GridRowCount) {
+            var downItem = gridItemMap[GetIndex(rowIndex + 1, columnIndex)];
+            item.AddNeighbor(downItem);
+        }
+        
+        // 左
+        if (columnIndex - 1 >= 0) {
+            var leftItem = gridItemMap[GetIndex(rowIndex, columnIndex - 1)];
+            item.AddNeighbor(leftItem);
+        }
+        
+        // 右
+        if (columnIndex + 1 < GridColumnCount) {
+            var rightItem = gridItemMap[GetIndex(rowIndex, columnIndex + 1)];
+            item.AddNeighbor(rightItem);
+        }
     }
 }
